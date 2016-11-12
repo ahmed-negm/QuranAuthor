ad83aff3c13575623a7445eaef6656968f6128f6
ahmed-negm <anegm@icinetic.com>
2016-11-12 15:30:14 +0200

    Add SnippetWindow

--- QuranAuthor/MainWindow.xaml.cs
+++ QuranAuthor/MainWindow.xaml.cs
@@ -1,57 +1,18 @@
-﻿using QuranAuthor.Helps;
-using QuranAuthor.Repositories;
-using QuranAuthor.Services;
-using System;
-using System.Drawing;
-using System.Windows;
+﻿using System.Windows;
 
 namespace QuranAuthor
 {
     public partial class MainWindow : Window
     {
-        private ClipboardHelper clipboardHelper;
-
         public MainWindow()
         {
             InitializeComponent();
-            this.clipboardHelper = new ClipboardHelper(this);
-            this.clipboardHelper.ItemCopied += ClipboardHelper_ItemCopied;
         }
 
-        private void ClipboardHelper_ItemCopied(object sender, ItemCopiedEventArgs e)
+        private void takeSnippet_Click(object sender, RoutedEventArgs e)
         {
-            var snippetService = new SnippetService();
-
-            var snippet = snippetService.ExtractSnippet(e.Rtf);
-
-            var bitmap = WindowCapturer.Capture();
-
-            var selection = BitmapHelper.GetSnippetSelection(bitmap);
-
-            snippet = BitmapHelper.CalculatePageSelection(snippet, selection);
-
-            SnippetRepository snippetRepository = new SnippetRepository();
-            snippetRepository.AddSnippet(snippet);
-
-            var bmp = new Bitmap(@"E:\Fun\Tafseer\Images\Nexus 9\final\" + snippet.Page + ".png");
-            var rect = new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height);
-
-            var upgradedBmp = bmp.Clone(rect, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-            bmp.Dispose();
-
-            Bitmap page = BitmapHelper.FocusSelection(upgradedBmp, snippet);
-
-            page.Save("E://page.png", System.Drawing.Imaging.ImageFormat.Png);
-        }
-
-        private void Window_Closed(object sender, EventArgs e)
-        {
-            this.clipboardHelper.Stop();
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            this.clipboardHelper.Start();
+            SnippetWindow snipetWindow = new SnippetWindow();
+            snipetWindow.Show();
         }
     }
 }
---
+++ QuranAuthor/SnippetWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using QuranAuthor.Helps;
+using QuranAuthor.Repositories;
+using QuranAuthor.Services;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace QuranAuthor
+{
+
+    public partial class SnippetWindow : Window
+    {
+
+        private ClipboardHelper clipboardHelper;
+        private SnippetService snippetService = new SnippetService();
+        private SnippetRepository snippetRepository = new SnippetRepository();
+
+        public SnippetWindow()
+        {
+            InitializeComponent();
+            this.clipboardHelper = new ClipboardHelper(this);
+            this.clipboardHelper.ItemCopied += ClipboardHelper_ItemCopied;
+        }
+
+        private void ClipboardHelper_ItemCopied(object sender, ItemCopiedEventArgs e)
+        {
+            var snippet = snippetService.ExtractSnippet(e.Rtf);
+
+            var bitmap = WindowCapturer.Capture();
+
+            var selection = BitmapHelper.GetSnippetSelection(bitmap);
+
+            snippet = BitmapHelper.CalculatePageSelection(snippet, selection);
+
+            //snippetRepository.AddSnippet(snippet);
+
+            var bmp = new Bitmap(@"E:\Fun\Tafseer\Images\Nexus 9\final\" + snippet.Page + ".png");
+            var rect = new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height);
+
+            var upgradedBmp = bmp.Clone(rect, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            bmp.Dispose();
+
+            Bitmap page = BitmapHelper.FocusSelection(upgradedBmp, snippet);
+
+            //page.Save("E://page.png", System.Drawing.Imaging.ImageFormat.Png);
+
+            imgPage.Source = BitmapToImageSource(page);
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            this.clipboardHelper.Stop();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.clipboardHelper.Start();
+        }
+
+        BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Png);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+    }
+}
